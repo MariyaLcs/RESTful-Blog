f0eabb246a9d09331c900035aee19aacd9be1f77
Mariya Lucas <mariya.podosinova@gmail.com>
2020-01-30 12:37:08 +0000

    Create the Blog Model

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+var blogChema = new mongoose.Schema({
+  title: String,
+  image: String,
+  body: String,
+  created: { type: Date, default: Date.now }
+});
+var Blog = mongoose.model("Blog", blogChema);
+
 // title
 // image
 // body
