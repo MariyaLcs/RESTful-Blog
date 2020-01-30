ba449e72ac1eb0aafab63e7ddf1506079e28718f
Mariya Lucas <mariya.podosinova@gmail.com>
2020-01-30 12:49:41 +0000

    Cheking the route with Blog.create()

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ var express = require("express"),
   mongoose = require("mongoose"),
   app = express();
 
+//APP Config
 mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//Mongoose/Model Config
 var blogChema = new mongoose.Schema({
   title: String,
   image: String,
@@ -16,10 +18,13 @@ var blogChema = new mongoose.Schema({
 });
 var Blog = mongoose.model("Blog", blogChema);
 
-// title
-// image
-// body
-// created
+Blog.create({
+  title: "Green Park",
+  image:
+    "https://images.unsplash.com/photo-1446844805183-9f5af45f89ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80",
+  body: "Hello this is a blog post"
+});
+//RESTful Routes
 
 app.listen(process.env.PORT || 3000, function() {
   console.log("Server has started!");
