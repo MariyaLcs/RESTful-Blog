a1311e1155841101f28bf05efef792deb03f2e58
Mariya Lucas <mariya.podosinova@gmail.com>
2020-01-31 09:48:48 +0000

    Add Edit Link

--- app.js
+++ app.js
@@ -90,7 +90,13 @@ app.put("/blogs/:id", function(req, res) {
 
 //Delete ROUTE
 app.delete("/blogs/:id", function(req, res) {
-  res.send("YOU HAVE REACHED THE DESTROY ROUTE");
+  Blog.findByIdAndRemove(req.params.id, function(err) {
+    if (err) {
+      res.redirect("/blogs");
+    } else {
+      res.redirect("/blogs");
+    }
+  });
 });
 
 app.listen(process.env.PORT || 3000, function() {
